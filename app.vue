55d3462079cc2514874fcaa669672575d6ebe9ee
sureshkrishnanv <sureshkrishnanv@outlook.com>
2025-01-05 10:47:22 +0530

    Changed App.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <span class="portfolio" >Welcome to My PortFolio</span>
   </div>
 </template>
+
+<style>
+.portfolio {
+  font-size: 200px;
+  color: red;
+  font-family: 'Arial';
+  text-align: center;
+  margin-top: 200px;
+  
+}
+</style>
